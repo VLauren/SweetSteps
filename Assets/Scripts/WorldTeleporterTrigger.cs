3354cb10033c44de773f662476998fcc1304942a
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2022-07-15 10:07:46 +0200

    movimiento entre mundos, progreso completo jugable

---
+++ Assets/Scripts/WorldTeleporterTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldTeleporterTrigger : MonoBehaviour
+{
+    public int World;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<MainChar>() != null )
+        {
+            if(GameData.IsWorldUnlocked(World) && World != GameData.CurrentWorld)
+            {
+                GameData.SetWorld(World);
+                SceneManager.LoadScene("HubScene");
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+
+        Gizmos.color = new Color(0.75f, 0.5f, 1, 0.5f);
+        Gizmos.DrawCube(Vector3.zero, Vector3.one);
+        Gizmos.color = new Color(0.5f, 0, 1, 0.7f);
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
+}
