7dc820b8ff47bc699e7bff7a8696c73b9c91c11a
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2022-08-05 17:00:07 +0200

    logica de posicion de cursor de editor

--- Assets/Scripts/LevelEditor/LevelEditor.cs
+++ Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -12,17 +12,32 @@ public class LevelEditor : MonoBehaviour
     public GameObject LevelEndPrefab;
     public GameObject WallPrefab;
 
+    [Space()]
+    public GameObject CursorPrefab;
+
+    Transform EditorCursor;
+
+    void Start()
+    {
+        EditorCursor = Instantiate(CursorPrefab).transform;
+    }
+
     void Update()
     {
         Mouse mouse = Mouse.current;
 
         Ray ray = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
+        Plane floor = new Plane(Vector3.up, 0);
+        float enter = 0;
+        Vector3 hitPt = new Vector3();
+        if (floor.Raycast(ray, out enter))
+            hitPt = ray.GetPoint(enter);
 
-        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.position + ray.direction * 100, Color.red);
+        hitPt.x = 3 * Mathf.RoundToInt((hitPt.x - 1.5f) / 3) + 1.5f;
+        hitPt.z = 3 * Mathf.RoundToInt((hitPt.z - 1.5f) / 3) + 1.5f;
 
-        // - interseccion con el plano y debug
-        // - redondear a casilla
-        // - ver algun marcador de casilla
+        EditorCursor.position = hitPt;
+
         // - instanciar algo con teclas
         // - mantener referencias a lo instanciado, array bidimensional
         // - borrar/sustituir
