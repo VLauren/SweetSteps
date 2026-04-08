2714159459766efb4d5d96a037a27bd10345c1c3
VLauren <laurentino.vilchez@gmail.com>
2026-04-08 09:25:57 +0200

    level text indicator

---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    void Start()
+    {
+        if (!SceneManager.GetActiveScene().name.Equals("LevelPlayScene"))
+        {
+            transform.Find("LevelNumber").gameObject.SetActive(false);
+        }
+        else
+        {
+            int level = GameData.CurrentLevel;
+            int total = LevelsData.DoorLevelCount(GameData.CurrentWorld, GameData.CurrentDoor);
+            transform.Find("LevelNumber").GetComponent<Text>().text = level + "/" + total;
+        }
+    }
+}
