f1d08bc828ae247e63ab7f249611539860f1a21e
VLauren <laurentino.vilchez@gmail.com>
2026-04-28 08:41:32 +0200

    stars  improvements

--- Assets/Scripts/StarMovement.cs
+++ Assets/Scripts/StarMovement.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class StarMovement : MonoBehaviour
 {
-    float RotationSpeed = 0.4f;
-    float OscSpeed = 0.4f;
-    float Oscilation = 0.002f;
+    float RotationSpeed = 0.2f;
+    float OscSpeed = 0.3f;
+    float Oscilation = 0.3f;
+
+    Vector3 StartPos;
+    float StartOsc;
+
+    void Start()
+    {
+        StartPos = transform.position;
+        StartOsc = Random.value;
+    }
 
     void Update()
     {
         transform.Rotate(new Vector3(0, Time.deltaTime * RotationSpeed * 360, 0));
-        transform.Translate(0, Oscilation * Mathf.Sin(OscSpeed * Mathf.PI * Time.time), 0);
+        transform.position = StartPos + transform.up * Oscilation * Mathf.Sin(Mathf.PI * 2 * (OscSpeed * Time.time + StartOsc));
     }
 }
 
